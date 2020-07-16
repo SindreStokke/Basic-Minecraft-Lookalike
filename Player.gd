362715db708b34ee9862385dfb497bd4ef59de20
Sindre Stokke <sindrestokke@gmail.com>
2020-07-17 00:00:27 +0200

    Arbeid gjort 2020.07.14

--- Player.gd
+++ Player.gd
@@ -2,45 +2,60 @@ extends KinematicBody
 
 
 # Declare member variables here.
-var fart = Vector3(0,0,0)
-var banefart = 5
-var gravity = 9.8
-var jump_height = gravity*0.8
+var akselerasjon = 20
+var banefart = 7
+var run_factor = 1.6
+var gravity = 9.8*4
+var jump_height = gravity*0.4
 
+var mouse_sensitivity = 0.05
+
+var fart = Vector3()
+var direction = Vector3()
+var fall = Vector3()
+
+onready var head = $Head
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _physics_process(delta):
 	movement(delta)
 
 func _input(event):
-	movement_input()
-
-func movement_input():
-	if Input.is_action_pressed("ui_down"):
-		fart.z = banefart
-	if Input.is_action_just_released("ui_down"):
-		fart.z = 0
-	if Input.is_action_pressed("ui_up"):
-		fart.z = -banefart
-	if Input.is_action_just_released("ui_up"):
-		fart.z = 0
-	if Input.is_action_pressed("ui_right"):
-		fart.x = banefart
-	if Input.is_action_just_released("ui_right"):
-		fart.x = 0
-	if Input.is_action_pressed("ui_left"):
-		fart.x = -banefart
-	if Input.is_action_just_released("ui_left"):
-		fart.x = 0
-	if Input.is_action_just_pressed("space") and is_on_floor():
-		fart.y = jump_height
+	if event is InputEventMouseMotion:
+		rotate_y(deg2rad(-event.relative.x * mouse_sensitivity))
+		head.rotate_x(deg2rad(-event.relative.y * mouse_sensitivity))
+		head.rotation.x = clamp(head.rotation.x, deg2rad(-90), deg2rad(90))
 
 func movement(delta):
+	direction = Vector3()
+	
 	if not is_on_floor():
-		fart.y -= gravity*delta
-	move_and_slide(fart, Vector3.UP)
+		fall.y -= gravity*delta
+	if Input.is_action_just_pressed("space") and is_on_floor():
+		fall.y = jump_height
+	
+	if Input.is_action_just_pressed("ui_cancel"):
+		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+	if Input.is_action_pressed("move_forward"):
+		direction -= transform.basis.z
+	if Input.is_action_pressed("move_backward"):
+		direction += transform.basis.z
+	if Input.is_action_pressed("move_left"):
+		direction -= transform.basis.x
+	if Input.is_action_pressed("move_right"):
+		direction += transform.basis.x
+	if Input.is_action_just_pressed("crouch"):
+		banefart = banefart*run_factor
+	if Input.is_action_just_released("crouch"):
+		banefart = banefart/run_factor
+	
+	direction = direction.normalized()
+	fart = fart.linear_interpolate(direction * banefart, akselerasjon * delta)
+	fart = move_and_slide(fart, Vector3.UP)
+	move_and_slide(fall, Vector3.UP)
--- Verden.gd
+++ Verden.gd
@@ -4,26 +4,72 @@ extends Node
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-var rendredistanse = 8
-var rendre_height_min = -6
+var rendredistanse = 10
+var worldgen_noise = null
+var overflate_height = 10
+var underflate_height = 4
+var blokker = ["res://blokker/dirt.tscn", "res://blokker/grass.tscn", "res://blokker/stone.tscn", "res://blokker/sand.tscn"]
+var strukturer = ["res://strukturer/tree_oak.tscn"]
+var blokk = null
+var forrige_helkoordinater = [0,0]
+var nytt_objekt = null
+var trehyppighet = 60 # Det gjennomsnittlige antallet topp-blokker som uten trær
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	# Definerer verdengenereringens støykart
+	randomize()
+	worldgen_noise = OpenSimplexNoise.new()
+	worldgen_noise.seed = randi()
+	worldgen_noise.octaves = 2
+	worldgen_noise.period = 100
+	worldgen_noise.lacunarity = 15
+	worldgen_noise.persistence = 0.75
+	
 	generer_terreng()
-	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _process(delta):
+	# Rendrer inn nytt terreng når spilleren går
+	if floor(get_node("Player").global_transform.origin.x) != forrige_helkoordinater[0]:
+		pass
 
 func generer_terreng():
 	for nr_x in 2*rendredistanse:
 		for nr_z in 2*rendredistanse:
-			var height = 0
-			while height > rendre_height_min:
-				var nytt_objekt = load("res://Block.tscn").instance() # Oppretter det nye objektet
-				nytt_objekt.transform.origin = Vector3((nr_x-rendredistanse)*2,height,(nr_z-rendredistanse)*2) # Endrer posisjonen til det nye objektet.
+			
+			# Setter terrengets overflate-høyde på gitte x og z koordinater
+			var height = int((round(overflate_height*worldgen_noise.get_noise_2d(nr_x,nr_z))+overflate_height)/2)
+			if height % 2 == 1:
+				height+= 1 # Øker høyden med 1 dersom den er i halvblokkhøyde.
+			
+			# Flytter spilleren opp til overflaten
+			if nr_x == 0 and nr_z == 0:
+				get_node("Player").global_transform.origin = Vector3(0,height+10.5,0)
+			
+			# Genererer overflaten
+			var h = height
+			while h >= 0:
+				if h == height:
+					blokk = blokker[1]
+					# Genererer et "oak tree" med en viss sannsynlighet
+					if rand_range(0,trehyppighet+1) >= trehyppighet:
+						nytt_objekt = load(strukturer[0]).instance()
+						nytt_objekt.transform.origin = Vector3((nr_x-rendredistanse)*2,h+1,(nr_z-rendredistanse)*2)
+						get_node("Strukturer").add_child(nytt_objekt)
+				else:
+					blokk = blokker[0]
+				# Genererer gress/ jord-blokkene
+				nytt_objekt = load(blokk).instance() # Oppretter det nye objektet
+				nytt_objekt.transform.origin = Vector3((nr_x-rendredistanse)*2,h,(nr_z-rendredistanse)*2) # Endrer posisjonen til det nye objektet.
 				get_node("Blokker").add_child(nytt_objekt) # Legger til det nye objektet på ønsket sted i det totale scene-hierarkiet relativt til der denne koden kjøres.
-				height -= 2
+				h -= 2
+			
+			# Genererer underflaten
+			blokk = blokker[2]
+			for nr_y in underflate_height:
+				nytt_objekt = load(blokk).instance() # Oppretter det nye objektet
+				nytt_objekt.transform.origin = Vector3((nr_x-rendredistanse)*2,-nr_y,(nr_z-rendredistanse)*2) # Endrer posisjonen til det nye objektet.
+				get_node("Blokker").add_child(nytt_objekt) # Legger til det nye objektet på ønsket sted i det totale scene-hierarkiet relativt til der denne koden kjøres.
